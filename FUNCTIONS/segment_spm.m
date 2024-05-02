b43d8616ca6b8de440f5ae92c86132601a93a544
fabiub51 <154235429+fabiub51@users.noreply.github.com>
2024-05-02 11:31:59 +0200

    Add files via upload

---
+++ FUNCTIONS/segment_spm.m
@@ -0,0 +1,13 @@
+function segment_spm(x)
+% List of open inputs
+nrun = 1; % enter the number of runs here
+y = 'auditory/jobs/segment_job.m';
+jobfile = {append(x,y)};
+jobs = repmat(jobfile, 1, nrun);
+inputs = cell(0, nrun);
+for crun = 1:nrun
+end
+spm('defaults', 'FMRI');
+spm_jobman('run', jobs, inputs{:});
+
+end
