47e8cb23ff56581cf53f453a9909e8533d36cfde
fabiub51 <154235429+fabiub51@users.noreply.github.com>
2024-05-03 11:07:47 +0200

    Update main.m

--- MAIN SCRIPT/main.m
+++ MAIN SCRIPT/main.m
@@ -1,15 +1,22 @@
 %% Preprocessing %% 
 
 %add SPM12
-addpath(genpath([pwd,'/SPM12']))
+addpath(genpath([pwd,'/SPM12'])) %This ensures your SPM12 is loaded and running
 
 %Vector indicating which steps should be performed
-v = [1,1,1,1,1];
+% v(1) = Realignment
+% v(2) = Coregistration
+% v(3) = Segmentation 
+% v(4) = Normalization 
+% v(5) = Smoothing 
+v = [1,1,1,1,1]; 
+
+%Switch-case statements determine which jobs have to be executed 
 
 %% Establishing directories where the data are
-data_dir = '/Users/fabiusberner/Documents/MATLAB/auditory/MoAEpilot/fM00223';
-source_dir = '/Users/fabiusberner/Documents/MATLAB/auditory/MoAEpilot/sM00223';
-tpm_path = '/Users/fabiusberner/Documents/MATLAB/SPM12/tpm';
+data_dir = '/Users/fabiusberner/Documents/MATLAB/auditory/MoAEpilot/fM00223'; %This is the directory of the fMRI .img files 
+source_dir = '/Users/fabiusberner/Documents/MATLAB/auditory/MoAEpilot/sM00223'; %This is the directory of the source image for coregistration
+tpm_path = '/Users/fabiusberner/Documents/MATLAB/SPM12/tpm'; %This is the directory for NIFTI files for segmentation
 
 %% Realignment 
 switch v(1)
