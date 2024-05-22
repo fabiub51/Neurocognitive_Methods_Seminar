4f10c08aafbccbaffff56ca2cb8e29e9a9ba7dcb
fabiub51 <154235429+fabiub51@users.noreply.github.com>
2024-05-22 17:47:12 +0200

    Update normalise_pre.m

--- FUNCTIONS/normalise_pre.m
+++ FUNCTIONS/normalise_pre.m
@@ -1,8 +1,8 @@
-function normalise_pre(source_dir)
+function normalise_pre(data_dir,source_dir)
 
 normalise = struct; 
-files_n = spm_select('List',source_dir,'^ms','.nii');
-fs_n = cellstr([repmat([source_dir filesep], size(files_n,1), 1) files_n, repmat(',1',size(files_n,1),1)]);
+files_n = spm_select('List',data_dir,'^fM','.img');
+fs_n = cellstr([repmat([data_dir filesep], size(files_n,1), 1) files_n, repmat(',1',size(files_n,1),1)]);
 def_file = spm_select('List',source_dir,'^y','.nii');
 def_n = cellstr([repmat([source_dir filesep], size(def_file,1), 1) def_file]);
         
