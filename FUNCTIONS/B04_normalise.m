821e500f1dba5bc9740be5b0697b9db43662256a
fabiub51 <154235429+fabiub51@users.noreply.github.com>
2024-05-30 16:33:21 +0200

    Add files via upload

---
+++ FUNCTIONS/B04_normalise.m
@@ -0,0 +1,19 @@
+function B04_normalise(data_dir,source_dir)
+
+normalise = struct; 
+files_n = spm_select('List',data_dir,'^M','.nii');
+fs_n = cellstr([repmat([data_dir filesep], size(files_n,1), 1) files_n, repmat(',1',size(files_n,1),1)]);
+def_file = spm_select('List',source_dir,'^y','.nii');
+def_n = cellstr([repmat([source_dir filesep], size(def_file,1), 1) def_file]);
+        
+normalise.matlabbatch{1}.spm.spatial.normalise.write.subj.def = def_n;
+normalise.matlabbatch{1}.spm.spatial.normalise.write.subj.resample = fs_n;
+normalise.matlabbatch{1}.spm.spatial.normalise.write.woptions.bb = [-78 -112 -70
+                                                                  78 76 85];
+normalise.matlabbatch{1}.spm.spatial.normalise.write.woptions.vox = [1 1 3];
+normalise.matlabbatch{1}.spm.spatial.normalise.write.woptions.interp = 4;
+normalise.matlabbatch{1}.spm.spatial.normalise.write.woptions.prefix = 'w';
+
+spm_jobman('run', normalise.matlabbatch);
+
+clear matlabbatch
---
+++ FUNCTIONS/B05_smoothing.m
@@ -0,0 +1,14 @@
+function B05_smoothing(data_dir)
+
+smooth = struct; 
+files_w = spm_select('List',data_dir,'^w','.nii');
+fs_w = cellstr([repmat([data_dir filesep], size(files_w,1), 1) files_w, repmat(',1',size(files_w,1),1)]);
+          
+smooth.matlabbatch{1}.spm.spatial.smooth.data = fs_w;
+smooth.matlabbatch{1}.spm.spatial.smooth.fwhm = [6 6 6];
+smooth.matlabbatch{1}.spm.spatial.smooth.dtype = 0;
+smooth.matlabbatch{1}.spm.spatial.smooth.im = 0;
+smooth.matlabbatch{1}.spm.spatial.smooth.prefix = 's';
+spm_jobman('run', smooth.matlabbatch);
+
+clear matlabbatch
