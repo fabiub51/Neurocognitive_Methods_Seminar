740fe42616d7eb0638570d53c839330086a868e6
fabiub51 <154235429+fabiub51@users.noreply.github.com>
2024-06-07 15:09:07 +0200

    Update B01_realignment.m

--- FUNCTIONS/Bistable_FUNCTIONS/B01_realignment.m
+++ FUNCTIONS/Bistable_FUNCTIONS/B01_realignment.m
@@ -1,11 +1,23 @@
-function B01_realignment(data_dir)
+function B01_realignment(data_dir,no_runs)
 
+file_dir = {};
+for r = 1:no_runs
+    if no_runs < 10
+        file_dir{r} = strcat(data_dir,num2str(0),num2str(r));
+    else
+        file_dir{r} = strcat(data_dir,num2str(r));
+    end
+end
 
-realign_estimate_reslice = struct; 
-files = spm_select('List', data_dir, '^M','.nii');
-fs = cellstr([repmat([data_dir filesep], size(files,1), 1) files, repmat(',1',size(files,1),1)]);
+for f = 1:no_runs
+    fs = spm_select('List', file_dir{f}, '^M','.nii');
+    files = cellstr([repmat([file_dir{f} filesep], size(fs,1), 1) fs, repmat(',1',size(fs,1),1)]);
+    data{f} = files;
+
+end 
 %Eoptions
-realign_estimate_reslice.matlabbatch{1}.spm.spatial.realign.estwrite.data = {fs};
+realign_estimate_reslice.matlabbatch{1}.spm.spatial.realign.estwrite.data = data;
+%%
 realign_estimate_reslice.matlabbatch{1}.spm.spatial.realign.estwrite.eoptions.quality = 0.9;
 realign_estimate_reslice.matlabbatch{1}.spm.spatial.realign.estwrite.eoptions.sep = 4;
 realign_estimate_reslice.matlabbatch{1}.spm.spatial.realign.estwrite.eoptions.fwhm = 5;
@@ -13,8 +25,7 @@ realign_estimate_reslice.matlabbatch{1}.spm.spatial.realign.estwrite.eoptions.rt
 realign_estimate_reslice.matlabbatch{1}.spm.spatial.realign.estwrite.eoptions.interp = 2;
 realign_estimate_reslice.matlabbatch{1}.spm.spatial.realign.estwrite.eoptions.wrap = [0 0 0];
 realign_estimate_reslice.matlabbatch{1}.spm.spatial.realign.estwrite.eoptions.weight = '';
-%Roptions 
-realign_estimate_reslice.matlabbatch{1}.spm.spatial.realign.estwrite.roptions.which = [0 1];
+realign_estimate_reslice.matlabbatch{1}.spm.spatial.realign.estwrite.roptions.which = [2 1];
 realign_estimate_reslice.matlabbatch{1}.spm.spatial.realign.estwrite.roptions.interp = 4;
 realign_estimate_reslice.matlabbatch{1}.spm.spatial.realign.estwrite.roptions.wrap = [0 0 0];
 realign_estimate_reslice.matlabbatch{1}.spm.spatial.realign.estwrite.roptions.mask = 1;
