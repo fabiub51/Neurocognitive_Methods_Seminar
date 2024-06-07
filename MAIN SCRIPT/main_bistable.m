69ca567e23e4ce9193e986f4e8dadd32ed3043f2
fabiub51 <154235429+fabiub51@users.noreply.github.com>
2024-06-07 17:23:07 +0200

    Update main_bistable.m

--- MAIN SCRIPT/main_bistable.m
+++ MAIN SCRIPT/main_bistable.m
@@ -12,12 +12,22 @@ addpath(genpath([pwd,'/SPM12'])) %This ensures your SPM12 is loaded and running
 v = [1,1,1,1,1]; 
 
 %all participants
-subs = 4;
-runs = [1,2,3,4,5,6,7];
+subs = [1,3,4]; 
+runs = [1,2,3,4,5,6,7]; % These are the 6 functional runs + the localizer
 
 %% Establishing directories where the data are
 tpm_path = '/Users/fabiusberner/Documents/MATLAB/SPM12/tpm';
 pre_data_dir = '/Users/fabiusberner/Documents/UNI/2. Semester/NMDA practical /Bistable_perception/Derivatives/sub-00';
+
+% This is the folder structure how I understood BIDS: 
+% Bistable perception (Project) 
+    % Raw Data - how the data come out of the scanner (dcm files)
+        % sub-00x - folder for every participant
+    % Derivatives - any manipulation that was done to the data
+        % sub-00x - folder for every participant
+            % anat - any type of anatomical image
+            % func - functional data
+                % run-0x - all of the runs (from one to 7) 
 %% Realignment 
 clear matlabbatch 
 
@@ -32,8 +42,8 @@ end
 %% Coregistration
 
 for i = 1:length(subs)
-        ref_dir = strcat('/Users/fabiusberner/Documents/UNI/2. Semester/NMDA practical /Bistable_perception/Derivatives/sub-00',num2str(subs(i)),'/func/run-01');
-        source_dir = strcat('/Users/fabiusberner/Documents/UNI/2. Semester/NMDA practical /Bistable_perception/Derivatives/sub-00',num2str(subs(i)),'/anat');
+        ref_dir = strcat(pre_data_dir,num2str(subs(i)),'/func/run-');
+        source_dir = strcat(pre_data_dir,num2str(subs(i)),'/anat');
         switch v(2)
             case 1
                 B02_coregister(ref_dir, source_dir);
@@ -41,8 +51,9 @@ for i = 1:length(subs)
         end
 end
 %% Segmentation
+
 for i = 1:length(subs)
-        source_dir = strcat('/Users/fabiusberner/Documents/UNI/2. Semester/NMDA practical /Bistable_perception/Derivatives/sub-00',num2str(subs(i)),'/anat');
+        source_dir = strcat(pre_data_dir,num2str(subs(i)),'/anat');
         switch v(3)
             case 1
                 B03_segment(tpm_path, source_dir);
@@ -53,8 +64,8 @@ end
 %% Normalize 
 
 for i = 1:length(subs)
-        data_dir = strcat('/Users/fabiusberner/Documents/UNI/2. Semester/NMDA practical /Bistable_perception/Derivatives/sub-00',num2str(subs(i)),'/func/run_');
-        source_dir = strcat('/Users/fabiusberner/Documents/UNI/2. Semester/NMDA practical /Bistable_perception/Derivatives/sub-00',num2str(subs(i)),'/anat');
+        data_dir = strcat(pre_data_dir,num2str(subs(i)),'/func/run-');
+        source_dir = strcat(pre_data_dir,num2str(subs(i)),'/anat');
         switch v(4)
             case 1
                 B04_normalise(data_dir,source_dir,length(runs));
@@ -64,12 +75,12 @@ end
 %% Smoothing
 
 for i = 1:length(subs)
-        data_dir = strcat('/Users/fabiusberner/Documents/UNI/2. Semester/NMDA practical /Bistable_perception/Derivatives/sub-00',num2str(subs(i)),'/func/Preprocessing/run_');
-        switch v(5)
-            case 1
-                B05_smoothing(data_dir,length(runs));
-            otherwise
-        end
+    data_dir = strcat(pre_data_dir,num2str(subs(i)),'/func/run-');    
+    switch v(5)
+        case 1
+            B05_smoothing(data_dir,length(runs));
+        otherwise
+    end
 end
 %% Specification - first level analysis 
 
