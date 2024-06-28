67c7581230d12a0ccdc61203a3e98ef69239071e
fabiub51 <154235429+fabiub51@users.noreply.github.com>
2024-06-28 11:08:59 +0200

    Update C01_specification_runs.m

--- FUNCTIONS/Bistable_FUNCTIONS/C01_specification_runs.m
+++ FUNCTIONS/Bistable_FUNCTIONS/C01_specification_runs.m
@@ -1,161 +1,99 @@
-function C01_specification_runs(func_dir,data_dir, runs, sub_no)
+%% Preprocessing %% 
 
-%% output directory
-cd(func_dir);
-mkdir('1st_level');
-output_dir = fullfile(func_dir, '1st_level');
-%% Create data 
-file_dir = {}; %creating the directory of the files for every run
-for r = 1:length(runs)
-    if runs < 10
-        file_dir{r} = strcat(data_dir,num2str(0),num2str(runs(r)));
-    else
-        file_dir{r} = strcat(data_dir,num2str(runs(r)));
+%add SPM12
+addpath(genpath([pwd,'/SPM12'])) %This ensures your SPM12 is loaded and running
+
+%Vector indicating which steps should be performed
+% v(1) = Realignment
+% v(2) = Coregistration
+% v(3) = Segmentation 
+% v(4) = Normalization 
+% v(5) = Smoothing 
+v = [1,1,1,1,1]; 
+
+%all participants
+subs = 1; 
+runs = [1,2,3,4,5,6,7]; % These are the 6 functional runs + the localizer
+
+%% Establishing directories where the data are
+tpm_path = 'C:\Users\berne\Documents\MATLAB\spm12\tpm';
+pre_data_dir = 'C:\Users\berne\Documents\Master FU\2. Semester\NMDA practical\Bistable_perception\Derivatives\sub-00';
+
+% This is the folder structure how I understood BIDS: 
+% Bistable perception (Project) 
+    % Raw Data - how the data come out of the scanner (dcm files)
+        % sub-00x - folder for every participant
+    % Derivatives - any manipulation that was done to the data
+        % sub-00x - folder for every participant
+            % anat - any type of anatomical image
+            % func - functional data
+                % run-0x - all of the runs (from one to 7) 
+%% Realignment 
+clear matlabbatch 
+
+for i = 1:length(subs)
+        data_dir = strcat(pre_data_dir,num2str(subs(i)),'/func/run-');
+        switch v(1)
+            case 1
+                B01_realignment(data_dir,length(runs),subs(i));
+            otherwise
+        end
+end
+%% Coregistration
+
+for i = 1:length(subs)
+        ref_dir = strcat(pre_data_dir,num2str(subs(i)),'/func/run-01');
+        source_dir = strcat(pre_data_dir,num2str(subs(i)),'/anat');
+        switch v(2)
+            case 1
+                B02_coregister(ref_dir, source_dir);
+            otherwise
+        end
+end
+%% Segmentation
+
+for i = 1:length(subs)
+        source_dir = strcat(pre_data_dir,num2str(subs(i)),'/anat');
+        switch v(3)
+            case 1
+                B03_segment(tpm_path, source_dir);
+            otherwise
+        end
+end
+
+%% Normalize 
+
+for i = 1:length(subs)
+        data_dir = strcat(pre_data_dir,num2str(subs(i)),'/func/run-');
+        source_dir = strcat(pre_data_dir,num2str(subs(i)),'/anat');
+        switch v(4)
+            case 1
+                B04_normalise(data_dir,source_dir,length(runs),subs(i));
+            otherwise
+        end
+end
+%% Smoothing
+
+for i = 1:length(subs)
+    data_dir = strcat(pre_data_dir,num2str(subs(i)),'/func/run-');    
+    switch v(5)
+        case 1
+            B05_smoothing(data_dir,length(runs),subs(i));
+        otherwise
     end
 end
 
-data = {};
-for f = 1:length(runs)
-    fs = spm_select('expand', fullfile(file_dir{f},strcat('swrsub-00',num2str(sub_no),'_task-BTP_run-0',num2str(runs(f)),'_bold.nii')));
-    files = cellstr(fs);
-    data{f} = files; % Building the cell array with all the filenames (7x360)
+%% Specification - first level analysis 
 
-end 
-%% Load onsets
-condition_1 = 'SIM';
-condition_2 = 'ALT';
-
-onsets = {};
-conditions = {};
-onsets_condition_1 = {};
-onsets_condition_2 = {};
-for l = 1:length(runs)
-    log_file = load(fullfile(file_dir{l}, strcat('log_run_0',num2str(runs(l)))));
-    onsets{l} = log_file.log.onset;
-    conditions{l} = log_file.log.conditions;
-
-    onsets_condition_1{l} = onsets{l}(find(conditions{l} == 1));
-    onsets_condition_2{l} = onsets{l}(find(conditions{l} == 2));
-
+for i = length(subs)
+    runs = [1 2 3 4 5 6];
+    data_dir = strcat(pre_data_dir,num2str(subs(i)),'/func/run-'); 
+    func_dir = strcat(pre_data_dir,num2str(subs(i)),'/func/');
+    C01_specification_runs(func_dir, data_dir, runs, subs(i));
 end 
 
-
-
-%% Load batch structure 
-matlabbatch{1}.spm.stats.fmri_spec.dir = {output_dir};
-matlabbatch{1}.spm.stats.fmri_spec.timing.units = 'secs';
-matlabbatch{1}.spm.stats.fmri_spec.timing.RT = 1;
-matlabbatch{1}.spm.stats.fmri_spec.timing.fmri_t = 16;
-matlabbatch{1}.spm.stats.fmri_spec.timing.fmri_t0 = 8;
-matlabbatch{1}.spm.stats.fmri_spec.sess(1).scans = data{1};
-matlabbatch{1}.spm.stats.fmri_spec.sess(1).cond(1).name = condition_1;
-matlabbatch{1}.spm.stats.fmri_spec.sess(1).cond(1).onset = onsets_condition_1{1};
-matlabbatch{1}.spm.stats.fmri_spec.sess(1).cond(1).duration = 8;
-matlabbatch{1}.spm.stats.fmri_spec.sess(1).cond(1).tmod = 0;
-matlabbatch{1}.spm.stats.fmri_spec.sess(1).cond(1).pmod = struct('name', {}, 'param', {}, 'poly', {});
-matlabbatch{1}.spm.stats.fmri_spec.sess(1).cond(1).orth = 1;
-matlabbatch{1}.spm.stats.fmri_spec.sess(1).cond(2).name = condition_2;
-matlabbatch{1}.spm.stats.fmri_spec.sess(1).cond(2).onset = onsets_condition_2{1};
-matlabbatch{1}.spm.stats.fmri_spec.sess(1).cond(2).duration = 8;
-matlabbatch{1}.spm.stats.fmri_spec.sess(1).cond(2).tmod = 0;
-matlabbatch{1}.spm.stats.fmri_spec.sess(1).cond(2).pmod = struct('name', {}, 'param', {}, 'poly', {});
-matlabbatch{1}.spm.stats.fmri_spec.sess(1).cond(2).orth = 1;
-matlabbatch{1}.spm.stats.fmri_spec.sess(1).multi = {''};
-matlabbatch{1}.spm.stats.fmri_spec.sess(1).regress = struct('name', {}, 'val', {});
-matlabbatch{1}.spm.stats.fmri_spec.sess(1).multi_reg = {''};
-matlabbatch{1}.spm.stats.fmri_spec.sess(1).hpf = 128;
-matlabbatch{1}.spm.stats.fmri_spec.sess(2).scans = data{2};
-matlabbatch{1}.spm.stats.fmri_spec.sess(2).cond(1).name = condition_1;
-matlabbatch{1}.spm.stats.fmri_spec.sess(2).cond(1).onset = onsets_condition_1{2};
-matlabbatch{1}.spm.stats.fmri_spec.sess(2).cond(1).duration = 8;
-matlabbatch{1}.spm.stats.fmri_spec.sess(2).cond(1).tmod = 0;
-matlabbatch{1}.spm.stats.fmri_spec.sess(2).cond(1).pmod = struct('name', {}, 'param', {}, 'poly', {});
-matlabbatch{1}.spm.stats.fmri_spec.sess(2).cond(1).orth = 1;
-matlabbatch{1}.spm.stats.fmri_spec.sess(2).cond(2).name = condition_2;
-matlabbatch{1}.spm.stats.fmri_spec.sess(2).cond(2).onset = onsets_condition_2{2};
-matlabbatch{1}.spm.stats.fmri_spec.sess(2).cond(2).duration = 8;
-matlabbatch{1}.spm.stats.fmri_spec.sess(2).cond(2).tmod = 0;
-matlabbatch{1}.spm.stats.fmri_spec.sess(2).cond(2).pmod = struct('name', {}, 'param', {}, 'poly', {});
-matlabbatch{1}.spm.stats.fmri_spec.sess(2).cond(2).orth = 1;
-matlabbatch{1}.spm.stats.fmri_spec.sess(2).multi = {''};
-matlabbatch{1}.spm.stats.fmri_spec.sess(2).regress = struct('name', {}, 'val', {});
-matlabbatch{1}.spm.stats.fmri_spec.sess(2).multi_reg = {''};
-matlabbatch{1}.spm.stats.fmri_spec.sess(2).hpf = 128;
-matlabbatch{1}.spm.stats.fmri_spec.sess(3).scans = data{1};
-matlabbatch{1}.spm.stats.fmri_spec.sess(3).cond(1).name = condition_1;
-matlabbatch{1}.spm.stats.fmri_spec.sess(3).cond(1).onset = onsets_condition_1{1};
-matlabbatch{1}.spm.stats.fmri_spec.sess(3).cond(1).duration = 8;
-matlabbatch{1}.spm.stats.fmri_spec.sess(3).cond(1).tmod = 0;
-matlabbatch{1}.spm.stats.fmri_spec.sess(3).cond(1).pmod = struct('name', {}, 'param', {}, 'poly', {});
-matlabbatch{1}.spm.stats.fmri_spec.sess(3).cond(1).orth = 1;
-matlabbatch{1}.spm.stats.fmri_spec.sess(3).cond(2).name = condition_2;
-matlabbatch{1}.spm.stats.fmri_spec.sess(3).cond(2).onset = onsets_condition_2{1};
-matlabbatch{1}.spm.stats.fmri_spec.sess(3).cond(2).duration = 8;
-matlabbatch{1}.spm.stats.fmri_spec.sess(3).cond(2).tmod = 0;
-matlabbatch{1}.spm.stats.fmri_spec.sess(3).cond(2).pmod = struct('name', {}, 'param', {}, 'poly', {});
-matlabbatch{1}.spm.stats.fmri_spec.sess(3).cond(2).orth = 1;
-matlabbatch{1}.spm.stats.fmri_spec.sess(3).multi = {''};
-matlabbatch{1}.spm.stats.fmri_spec.sess(3).regress = struct('name', {}, 'val', {});
-matlabbatch{1}.spm.stats.fmri_spec.sess(3).multi_reg = {''};
-matlabbatch{1}.spm.stats.fmri_spec.sess(3).hpf = 128;
-matlabbatch{1}.spm.stats.fmri_spec.sess(4).scans = data{2};
-matlabbatch{1}.spm.stats.fmri_spec.sess(4).cond(1).name = condition_1;
-matlabbatch{1}.spm.stats.fmri_spec.sess(4).cond(1).onset = onsets_condition_1{2};
-matlabbatch{1}.spm.stats.fmri_spec.sess(4).cond(1).duration = 8;
-matlabbatch{1}.spm.stats.fmri_spec.sess(4).cond(1).tmod = 0;
-matlabbatch{1}.spm.stats.fmri_spec.sess(4).cond(1).pmod = struct('name', {}, 'param', {}, 'poly', {});
-matlabbatch{1}.spm.stats.fmri_spec.sess(4).cond(1).orth = 1;
-matlabbatch{1}.spm.stats.fmri_spec.sess(4).cond(2).name = condition_2;
-matlabbatch{1}.spm.stats.fmri_spec.sess(4).cond(2).onset = onsets_condition_2{2};
-matlabbatch{1}.spm.stats.fmri_spec.sess(4).cond(2).duration = 8;
-matlabbatch{1}.spm.stats.fmri_spec.sess(4).cond(2).tmod = 0;
-matlabbatch{1}.spm.stats.fmri_spec.sess(4).cond(2).pmod = struct('name', {}, 'param', {}, 'poly', {});
-matlabbatch{1}.spm.stats.fmri_spec.sess(4).cond(2).orth = 1;
-matlabbatch{1}.spm.stats.fmri_spec.sess(4).multi = {''};
-matlabbatch{1}.spm.stats.fmri_spec.sess(4).regress = struct('name', {}, 'val', {});
-matlabbatch{1}.spm.stats.fmri_spec.sess(4).multi_reg = {''};
-matlabbatch{1}.spm.stats.fmri_spec.sess(4).hpf = 128;
-matlabbatch{1}.spm.stats.fmri_spec.sess(5).scans = data{1};
-matlabbatch{1}.spm.stats.fmri_spec.sess(5).cond(1).name = condition_1;
-matlabbatch{1}.spm.stats.fmri_spec.sess(5).cond(1).onset = onsets_condition_1{1};
-matlabbatch{1}.spm.stats.fmri_spec.sess(5).cond(1).duration = 8;
-matlabbatch{1}.spm.stats.fmri_spec.sess(5).cond(1).tmod = 0;
-matlabbatch{1}.spm.stats.fmri_spec.sess(5).cond(1).pmod = struct('name', {}, 'param', {}, 'poly', {});
-matlabbatch{1}.spm.stats.fmri_spec.sess(5).cond(1).orth = 1;
-matlabbatch{1}.spm.stats.fmri_spec.sess(5).cond(2).name = condition_2;
-matlabbatch{1}.spm.stats.fmri_spec.sess(5).cond(2).onset = onsets_condition_2{1};
-matlabbatch{1}.spm.stats.fmri_spec.sess(5).cond(2).duration = 8;
-matlabbatch{1}.spm.stats.fmri_spec.sess(5).cond(2).tmod = 0;
-matlabbatch{1}.spm.stats.fmri_spec.sess(5).cond(2).pmod = struct('name', {}, 'param', {}, 'poly', {});
-matlabbatch{1}.spm.stats.fmri_spec.sess(5).cond(2).orth = 1;
-matlabbatch{1}.spm.stats.fmri_spec.sess(5).multi = {''};
-matlabbatch{1}.spm.stats.fmri_spec.sess(5).regress = struct('name', {}, 'val', {});
-matlabbatch{1}.spm.stats.fmri_spec.sess(5).multi_reg = {''};
-matlabbatch{1}.spm.stats.fmri_spec.sess(5).hpf = 128;
-matlabbatch{1}.spm.stats.fmri_spec.sess(6).scans = data{2};
-matlabbatch{1}.spm.stats.fmri_spec.sess(6).cond(1).name = condition_1;
-matlabbatch{1}.spm.stats.fmri_spec.sess(6).cond(1).onset = onsets_condition_1{2};
-matlabbatch{1}.spm.stats.fmri_spec.sess(6).cond(1).duration = 8;
-matlabbatch{1}.spm.stats.fmri_spec.sess(6).cond(1).tmod = 0;
-matlabbatch{1}.spm.stats.fmri_spec.sess(6).cond(1).pmod = struct('name', {}, 'param', {}, 'poly', {});
-matlabbatch{1}.spm.stats.fmri_spec.sess(6).cond(1).orth = 1;
-matlabbatch{1}.spm.stats.fmri_spec.sess(6).cond(2).name = condition_2;
-matlabbatch{1}.spm.stats.fmri_spec.sess(6).cond(2).onset = onsets_condition_2{2};
-matlabbatch{1}.spm.stats.fmri_spec.sess(6).cond(2).duration = 8;
-matlabbatch{1}.spm.stats.fmri_spec.sess(6).cond(2).tmod = 0;
-matlabbatch{1}.spm.stats.fmri_spec.sess(6).cond(2).pmod = struct('name', {}, 'param', {}, 'poly', {});
-matlabbatch{1}.spm.stats.fmri_spec.sess(6).cond(2).orth = 1;
-matlabbatch{1}.spm.stats.fmri_spec.sess(6).multi = {''};
-matlabbatch{1}.spm.stats.fmri_spec.sess(6).regress = struct('name', {}, 'val', {});
-matlabbatch{1}.spm.stats.fmri_spec.sess(6).multi_reg = {''};
-matlabbatch{1}.spm.stats.fmri_spec.sess(6).hpf = 128;
-matlabbatch{1}.spm.stats.fmri_spec.fact = struct('name', {}, 'levels', {});
-matlabbatch{1}.spm.stats.fmri_spec.bases.hrf.derivs = [0 0];
-matlabbatch{1}.spm.stats.fmri_spec.volt = 1;
-matlabbatch{1}.spm.stats.fmri_spec.global = 'None';
-matlabbatch{1}.spm.stats.fmri_spec.mthresh = 0.8;
-matlabbatch{1}.spm.stats.fmri_spec.mask = {''};
-matlabbatch{1}.spm.stats.fmri_spec.cvi = 'AR(1)';
-
-%% Executing the job 
-spm_jobman('run', matlabbatch);
+%% Estimation
+for i = length(subs)
+    output_dir = strcat(func_dir,'1st_level');
+    C02_estimation(output_dir);
+end 
